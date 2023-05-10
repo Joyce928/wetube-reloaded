cef3ff2be3a80c767d9a8a7548c89f30f64b4abd
Joyce928 <92929006+Joyce928@users.noreply.github.com>
2023-05-10 21:47:43 +1000

    #13.1 Video Preview (05:05)

--- SRC/client/js/recorder.js
+++ SRC/client/js/recorder.js
@@ -1,11 +1,13 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
 const handleStart = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true,
+    audio: false,
+    video: true, // or here we can decide the size of the video stream like { width: 200, height: 200},
   });
-  console.log(stream);
+  video.srcObject = stream;
+  video.play();
 };
 
 startBtn.addEventListener("click", handleStart);
